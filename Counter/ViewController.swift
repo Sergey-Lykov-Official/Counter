10c1d1213c747e85157c256b8e5b270ee8b1c400
Sergey-Lykov-Official <serezhka.lykov@yandex.ru>
2023-03-23 16:20:57 +0300

    В проект были добавлены элементы (Label) и (Button) в main.storyboard, а также реализованы действия для этих элементов в viewController.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var couterLabel: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
+    var labelText = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        labelText+=1
+        couterLabel.text = "Значение счетчика: \(labelText)"
+    }
 }
 
